6f13e0702624c121b7b3cdd79f9f74374131ae1a
Sai Nikhil <176280075+sainikhil-sys@users.noreply.github.com>
2026-03-23 00:27:11 +0530

    feat: Auto-create superuser on deploy

--- build.sh
+++ build.sh
@@ -5,3 +5,9 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# Automatically create a default superuser without requiring interactive shell
+export DJANGO_SUPERUSER_PASSWORD=admin123
+export DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_EMAIL=admin@example.com
+python manage.py createsuperuser --noinput || true
